957b37313fe1102bdf04e156c91d41abdf6bf680
Darex León <dgleon1@espe.edu.ec>
2024-06-16 16:47:29 -0400

    Update Jenkinsfile.unit.groovy

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -5,68 +5,44 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                script {
-                    echo 'Iniciando etapa de compilación!'
-                    sh 'make build'
-                }
+                echo 'Building stage!'
+                sh 'make build'
             }
         }
         stage('Unit tests') {
             steps {
-                script {
-                    sh 'make test-unit'
-                    archiveArtifacts artifacts: 'results/*.xml'
-                }
+                sh 'make test-unit'
+                archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        stage('API tests') {
+        stage('Api tests') {
             steps {
-                script {
-                    sh 'make test-api'
-                    archiveArtifacts artifacts: 'results/*.xml'
-                }
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        stage('End-to-end tests') {
+        stage('e2e tests') {
             steps {
-                script {
-                    sh 'make test-e2e'
-                    archiveArtifacts artifacts: 'results/*.xml'
-                }
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        stage('Print logs') {
-            steps {
-                script {
-                    echo "Trabajo: ${env.JOB_NAME}"
-                    echo "Ejecución número: ${env.BUILD_NUMBER}"
-                }
-            }
-        }
+       
     }
     post {
         always {
-            script {
-                junit 'results/*_result.xml'
-            }
+            junit 'results/*_result.xml'
+            cleanWs()
         }
-        failure {
-            script {
-                def jobName = env.JOB_NAME
-                def buildNumber = env.BUILD_NUMBER
-                def recipient = "admaigualca@gmail.com"
-                def subject = "Pipeline fallido: ${jobName} #${buildNumber}"
-                def body = |
-                    <p>El pipeline <strong>${jobName} #${buildNumber}</strong> ha fallado.</p>
-                    <p>Revisar el estado del pipeline <a href="${env.BUILD_URL}">aquí</a>.</p>
-                    <p>Puedes ver los logs de la ejecución <a href="${env.BUILD_URL}console">aquí</a>.</p>
-                    <p>Saludos.</p>
-                emailext (
-                    to: recipient,
-                    subject: subject,
-                    body: body
-                )
-            }
-        }
+        success {  
+             echo "El nombre del trabajo es: ${JOB_NAME}"
+             echo "El numero de ejecucion es: ${BUILD_NUMBER}"
+             
+         }  
+        
+       failure {  
+             mail bcc: '', body: "<b>Example</b><br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "ERROR CI: Project name -> ${env.JOB_NAME}", to: "devdarex@gmail.com";  
+         }   
+        
     }
 }
